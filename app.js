7d17bb732ba02c7cbed5f569726cf30b70f70c0a
rahulrajdahal <rahulrajdahal@gmail.com>
2024-10-17 16:35:10 +0100

    build: adding numbers

--- app.js
+++ app.js
@@ -1,61 +1,10 @@
-// function DigitSum() {
-//   var number = document.getElementById("takeValue");
-//   var numberValue = number.value;
-//   var splitNum = numberValue.split("");
+const firstNumberBox = document.getElementById("firstNum");
+const secondNumberBox = document.getElementById("secondNum");
+const resultBox = document.getElementById("result");
+const addButton = document.querySelector(".form__button");
 
-//   var x = document.getElementById("para");
-
-//   parsedNum = splitNum.map(function (x) {
-//     return parseInt(x, 10);
-//   });
-
-//   var sum = parsedNum.reduce(function (a, b) {
-//     return a + b;
-//   }, 0);
-
-//   var firstSum = document.getElementById("firstSum");
-//   firstSum.value = sum;
-
-//   firstSumSplit = firstSum.value.split("");
-
-//   parsedSum = firstSumSplit.map(function (x) {
-//     return parseInt(x, 10);
-//   });
-
-//   var finalSum = parsedSum.reduce(function (a, b) {
-//     return a + b;
-//   }, 0);
-
-//   x.innerHTML = finalSum;
-
-//   if (sum === 7) {
-//     alert("success");
-//   } else {
-//     if (finalSum === 7) {
-//       alert("success");
-//     } else {
-//       alert("fail");
-//     }
-//   }
-// }
-
-function shortOne() {
-  var number = document.getElementById("takeValue").value.split("");
-  parsedNumber = number.map((x) => parseInt(x, 10));
-
-  var sum = parsedNumber.reduce((a, b) => a + b, 0);
-
-  secondNumber = document.getElementById("firstSum");
-  secondNumber.value = sum;
-  splitSecond = secondNumber.value.split("");
-
-  parsedSecond = splitSecond.map((x) => parseInt(x, 10));
-
-  finalSum = parsedSecond.reduce((a, b) => a + b, 0);
-
-  if (sum === 7 || finalSum === 7) {
-    alert("success");
-  } else {
-    alert("fail");
-  }
-}
+const addNumbers = () => {
+  const first = firstNumberBox.value;
+  const second = secondNumberBox.value;
+  resultBox.value = parseFloat(first) + parseFloat(second);
+};
