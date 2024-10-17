3a9a52a10f9b9db9d606ec7d683b3e85e418d43d
rahulrajdahal <rahulrajdahal@gmail.com>
2024-10-17 16:53:34 +0100

    build: readme

---
+++ Readme.md
@@ -0,0 +1,32 @@
+# [AddNumJS](https://github.com/rahulrajdahal/AddNum-js). Add any two numbers adn view the Results
+
+Add two numbers and instantly get the results.
+
+## Preview
+
+[![AddNumJS](./screenshots/add.png)](https://github.com/rahulrajdahal/AddNum-js)
+
+## 🏗 Development Guide
+
+### 1. clone the repository
+
+```sh
+git clone https://github.com/rahulrajdahal/AddNum-js.git
+```
+
+### 2. Open the project
+
+```sh
+open index.html in your browser
+```
+
+## 🚀 Project Structure
+
+Inside of project [AddNumJS](https://github.com/rahulrajdahal/AddNum-js), you'll see the following folders and files:
+
+```text
+/
+├── app.js
+├── styles.css
+└── index.html
+```
--- app.js
+++ app.js
@@ -3,8 +3,28 @@ const secondNumberBox = document.getElementById("secondNum");
 const resultBox = document.getElementById("result");
 const addButton = document.querySelector(".form__button");
 
+let first, second;
+
 const addNumbers = () => {
-  const first = firstNumberBox.value;
-  const second = secondNumberBox.value;
-  resultBox.value = parseFloat(first) + parseFloat(second);
+  first = firstNumberBox.value;
+  second = secondNumberBox.value;
+  resultBox.value = `The result is ${parseFloat(first) + parseFloat(second)}`;
+};
+
+const subtractNumbers = () => {
+  first = firstNumberBox.value;
+  second = secondNumberBox.value;
+  resultBox.value = `The result is ${parseFloat(first) - parseFloat(second)}`;
+};
+
+const multiplyNumbers = () => {
+  first = firstNumberBox.value;
+  second = secondNumberBox.value;
+  resultBox.value = `The result is ${parseFloat(first) * parseFloat(second)}`;
+};
+
+const divideNumbers = () => {
+  first = firstNumberBox.value;
+  second = secondNumberBox.value;
+  resultBox.value = `The result is ${parseFloat(first) / parseFloat(second)}`;
 };
